6ac35e318ff38cdafa5de0938b1c99788cfd31b2
adityasrc <196776132+adityasrc@users.noreply.github.com.>
2026-01-26 15:54:37 +0530

    feat: setup validation and db models

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,13 +2,26 @@ import express from "express";
 const app = express();
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
-import { JWT_SECRET } from "@repo/common/config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 // const JWT-SECRET: string = "dirtySecret";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
 
 app.use(express.json());
 //signup - for new account(register)
 
 app.post("/signup",function(req, res){
+
+    //.Parse just returns either data or error 
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    //safeParse returns three things, parsed data, true or false, error
+
+    if(!parsedData.success){
+        res.json({
+            message: "Incorrect inputs",
+            error: parsedData.error    
+        })
+        return;
+    }
     //db call here
     res.json({
        userId : "123" 
@@ -19,7 +32,15 @@ app.post("/signup",function(req, res){
 
 
 app.post("/signin", function(req, res){
-    //zod is left for now
+    //zod
+    const parsedData = SigninSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            message: "Incorrect inputs",
+            error: parsedData.error    
+        })
+        return;
+    }
     const userId = 1;
     const token = jwt.sign({
         userId
@@ -31,7 +52,16 @@ app.post("/signin", function(req, res){
 })
 
 app.post("/room", middleware, function(req, res){
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            message: "Incorrect inputs",
+            error: parsedData.error    
+        })
+        return;
+    }
     //db call
+
     res.json({
         roomId: "123"
     })
